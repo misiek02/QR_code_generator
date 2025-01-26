02301f8a21ee4cf28be44d034810111f12662a23
Michał Suski <95080436+misiek02@users.noreply.github.com>
2025-01-26 10:40:06 +0100

    Create QR_generator.py
    QR code generator - first commit

---
+++ QR_generator.py
@@ -0,0 +1,31 @@
+import qrcode
+import os
+
+# URL address
+data = "modnesciany.pl"
+
+# QR code generating
+qr = qrcode.QRCode(
+    version=1,  # QR size [1-40]
+    error_correction=qrcode.constants.ERROR_CORRECT_L,  # Correction level (L, M, Q, H)
+    box_size=10,  # Single square size
+    border=4,  # Border size
+)
+
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+def save_qr(img, base_name="qr"):
+    counter = 1
+    while True:
+        file_name = f"{base_name}#{counter}.png"
+        if not os.path.exists(file_name):
+            break
+        counter += 1
+
+    img.save(file_name)
+    print(f"QR saved as '{file_name}'.")
+
+save_qr(img)
